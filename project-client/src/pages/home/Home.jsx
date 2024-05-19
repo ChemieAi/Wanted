c1c47f68366afd0b54ac3482481da5a2f9266ce5
ChemieAi <kzlyburak@gmail.com>
2024-05-19 19:03:53 +0300

    SOME CHANGES

--- project-client/src/pages/home/Home.jsx
+++ project-client/src/pages/home/Home.jsx
@@ -34,12 +34,7 @@ function Home() {
           >
             Ürün Sat
                   </NavLink>
-                  <NavLink
-                      to={isLogged ? "/addProduct2" : "/login"}
-                      className="btn px-10 text-base rounded-2xl py-3 font-bold text-center flex justify-center items-center bg-white text-darkBlue hover:bg-darkBlue hover:text-white transition-all duration-150"
-                  >
-                      Talep Aç
-                  </NavLink>
+
         </div>
       </div>
       <div className="w-11/12 m-auto my-24">
@@ -121,84 +116,6 @@ function Home() {
             )}
           </div>
           </div>
-              <div className="text-3xl mb-5 font-bold">Güncel Talepler</div>
-              <div className="bg-gray-100 dark:bg-gray-700">
-                  <div className="grid grid-cols-12 gap-x-8 gap-y-10 sm:gap-y-10 md:gap-y-20">
-                      {products.map(
-                          (product, index) =>
-                              index < 8 &&
-                              !product.isSold && (
-                                  <NavLink
-                                      key={index}
-                                      className="flex flex-col justify-between rounded-md h-full  shadow-item mb-10 dark:bg-darkBlue dark:text-white col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3"
-                                      to={
-                                          isLogged
-                                              ? `/productdetails/${product.productId}`
-                                              : "/login"
-                                      }
-                                  >
-                                      <img
-                                          src={
-                                              product.imagePath
-                                                  ? apiImagesUrl + product.imagePath
-                                                  : defaultImage
-                                          }
-                                          className="rounded-t-md h-2/3 object-cover object-center w-full flex-shrink-0"
-                                          alt=""
-                                      />
-                                      <div className="text-center bg-white dark:bg-gray-800 flex flex-col h-full justify-between py-3 px-5">
-                                          <div className="flex justify-between">
-                                              <p>Ürün</p>
-                                              <p>{product.productName}</p>
-                                          </div>
-                                          <div className="flex justify-between">
-                                              <p>Kategori</p>
-                                              <p>{product.categoryName}</p>
-                                          </div>
-                                          {product.brandName && (
-                                              <div className="flex justify-between">
-                                                  <p>Marka</p>
-                                                  <p>{product.brandName}</p>
-                                              </div>
-                                          )}
-                                          {product.colorName && (
-                                              <div className="flex justify-between">
-                                                  <p>Renk</p>
-                                                  <p>{product.colorName}</p>
-                                              </div>
-                                          )}
-                                          <div className="flex justify-between">
-                                              <p>Fiyat</p>
-                                              <p>{product.price}₺</p>
-                                          </div>
-                                      </div>
-
-                                      <div>
-                                          {product.isOfferable ? (
-                                              <div className="py-1 bg-teal-500 text-white text-center text-sm">
-                                                  Teklif Verilebilir
-                                              </div>
-                                          ) : (
-                                              <div className="py-1 bg-indigo-500  text-white text-center text-sm">
-                                                  Teklif Verilemez
-                                              </div>
-                                          )}
-
-                                          {product.isSold ? (
-                                              <div className="py-1 bg-rose-500 rounded-b text-white text-center text-sm ">
-                                                  Satıldı
-                                              </div>
-                                          ) : (
-                                              <div className="py-1 bg-lime-500 rounded-b text-white text-center text-sm">
-                                                  Satılmadı
-                                              </div>
-                                          )}
-                                      </div>
-                                  </NavLink>
-                              )
-                      )}
-                  </div>
-              </div>
       </div>
     </div>
   );
